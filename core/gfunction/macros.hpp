c832972cb4c2f7d31404e806814a599b8e674039
pliduino <liduinopedro@gmail.com>
2025-08-08 22:46:50 -0300

    chore: formatted files

--- core/gfunction/macros.hpp
+++ core/gfunction/macros.hpp
@@ -4,33 +4,31 @@
 
 namespace GDGenInternal
 {
-    constexpr bool is_valid_gfunction_flag()
-    {
-        return true;
-    }
+constexpr bool is_valid_gfunction_flag() { return true; }
 
-    constexpr bool is_valid_gfunction_flag(const char *flag)
-    {
-        // constexpr const char *valid_flags[] = {
-        //     "CustomBindings",
-        //     "Resource",
-        // };
-        // constexpr size_t num_valid = sizeof(valid_flags) / sizeof(valid_flags[0]);
+constexpr bool is_valid_gfunction_flag(const char *flag)
+{
+    // constexpr const char *valid_flags[] = {
+    //     "CustomBindings",
+    //     "Resource",
+    // };
+    // constexpr size_t num_valid = sizeof(valid_flags) / sizeof(valid_flags[0]);
 
-        // for (size_t i = 0; i < num_valid; ++i)
-        // {
-        //     if (str_equal(flag, valid_flags[i]))
-        //         return true;
-        // }
+    // for (size_t i = 0; i < num_valid; ++i)
+    // {
+    //     if (str_equal(flag, valid_flags[i]))
+    //         return true;
+    // }
 
-        return false;
-    }
+    return false;
 }
+}  // namespace GDGenInternal
 
-#define CHECK_GFUNCTION_FLAG(flag) \
-    static_assert(GDGenInternal::is_valid_gfunction_flag(#flag), "Invalid GFUNCTION flag: " #flag);
+#define CHECK_GFUNCTION_FLAG(flag)                               \
+    static_assert(GDGenInternal::is_valid_gfunction_flag(#flag), \
+                  "Invalid GFUNCTION "                           \
+                  "flag: " #flag);
 
 #define VALIDATE_EACH_GFUNCTION_FLAG(...) FOR_EACH(CHECK_GFUNCTION_FLAG, SEMICOLON_END, __VA_ARGS__)
 
-#define GFUNCTION(...) \
-    VALIDATE_EACH_GFUNCTION_FLAG(__VA_ARGS__)
+#define GFUNCTION(...) VALIDATE_EACH_GFUNCTION_FLAG(__VA_ARGS__)
--- lib.hpp
+++ lib.hpp
@@ -1,19 +1,20 @@
 #pragma once
 
-#include "macro_helpers.hpp"
-
 #include "core/gclass/macros.hpp"
+#include "core/genum/macros.hpp"
+#include "core/gfunction/macros.hpp"
 #include "core/gproperty/macros.hpp"
 #include "core/gsignal/macros.hpp"
 #include "core/gstruct/macros.hpp"
-#include "core/genum/macros.hpp"
-#include "core/gfunction/macros.hpp"
+#include "macro_helpers.hpp"
 
 #define GODOT_GENERATOR_GLUE_(A, B) A##_##B
 #define GODOT_GENERATOR_GLUE(A, B) GODOT_GENERATOR_GLUE_(A, B)
 
 #ifdef GODOT_GENERATOR_EXPAND_MACROS
-#define GENERATED_BODY() GODOT_GENERATOR_GLUE(FILE_IDENTIFIER, GODOT_GENERATOR_GLUE(__LINE__, GENERATED_BODY()))
+#define GENERATED_BODY() \
+    GODOT_GENERATOR_GLUE(FILE_IDENTIFIER, GODOT_GENERATOR_GLUE(__LINE__, GENERATED_BODY()))
 #else
-#define GENERATED_BODY() GODOT_GENERATOR_GLUE(FILE_IDENTIFIER, GODOT_GENERATOR_GLUE(__LINE__, CORE_GENERATED_BODY()))
+#define GENERATED_BODY() \
+    GODOT_GENERATOR_GLUE(FILE_IDENTIFIER, GODOT_GENERATOR_GLUE(__LINE__, CORE_GENERATED_BODY()))
 #endif
